e18a0dd3a2c489ad95d027365a6bb829aa2c8051
Abdullah Abu Tasneem <abdullahabutasneem19@gmail.com>
2023-09-24 17:00:24 +0600

    Filter the option is added

--- src/Component/AppliedJobs/AppliedJobs.jsx
+++ src/Component/AppliedJobs/AppliedJobs.jsx
@@ -1,35 +1,51 @@
 import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { getStoredJobApplication } from "../../Utility/LocalStorage";
-import { list } from "postcss";
-
 
 const AppliedJobs = () => {
     const jobs = useLoaderData();
     const [appliedJobs, setAppliedJobs] = useState([]);
+    const [displayJobs, setDisplayJobs] = useState([]);
+
+    const handleDisplayJob = option => {
+        if(option === 'all') {
+            setDisplayJobs(appliedJobs);
+        }
+        else if(option === 'remote') {
+            const remoteJob = appliedJobs.filter(job => job.remote_or_onsite === 'Remote');
+            setDisplayJobs(remoteJob);
+        }
+        else if(option === 'onsite') {
+            const onsiteJob = appliedJobs.filter(job => job.remote_or_onsite === 'Onsite');
+            setDisplayJobs(onsiteJob);
+        }
+    }
+
     useEffect(()=> {
         const storedJobIds = getStoredJobApplication();
         if(jobs.length > 0) {
             const jobsApplied = jobs.filter(job => storedJobIds.includes(job.id));
             // console.log(jobsApplied);
             setAppliedJobs(jobsApplied);
+            setDisplayJobs(jobsApplied);
         }
         
-    },[])
+    },[jobs])
     return (
         <div>
             <h2>Jobs I applied: {appliedJobs.length}</h2>
-            <div className="dropdown">
-                <label tabIndex={0} className="btn m-1">Click</label>
-                <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-                    <li><a>All</a></li>
-                    <li><a>Onsite</a></li>
-                    <li><a>Remote</a></li>
+            
+            <details className="dropdown mb-32">
+                <summary className="m-1 btn">open or close</summary>
+                <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                    <li><a onClick={()=>handleDisplayJob('all')}>All</a></li>
+                    <li><a onClick={()=>handleDisplayJob('onsite')}>Onsite</a></li>
+                    <li><a onClick={()=>handleDisplayJob('remote')}>Remote</a></li>
                 </ul>
-            </div>
+            </details>
             <ul>
                 {
-                    appliedJobs.map(job => <li key={job.id}><span>{job.company_name}</span></li>)
+                    displayJobs.map(job => <li key={job.id}><span>{job.company_name} - {job.remote_or_onsite}</span></li>)
                 }
             </ul>
         </div>
